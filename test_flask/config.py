1e42ef89d0d281f0fcade87593766d2c8362a394
VyacheslawDekin <vyacheslaw.dekin@yandex.ru>
2022-06-26 21:57:08 +0300

    init git

---
+++ test_flask/config.py
@@ -0,0 +1,10 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = '34dfg657sdf9b87sdnvm,nxc65asdfb654'
+    DEBUG = True
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = "irytlakol@gmail.com"
+    MAIL_PASSWORD = "plitka13"
---
+++ test_flask/main/routes.py
@@ -0,0 +1,11 @@
+from flask import render_template, Blueprint
+
+
+main = Blueprint('main', __name__)
+
+
+@main.route("/")
+@main.route("/home")
+def home():
+    return render_template('home.html')
+
